29d75ef993e53b83d6fe28b87e4dfb20ed300798
bakulcilok <67635957+bakulcilok@users.noreply.github.com>
2021-12-25 02:57:13 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -24,9 +24,9 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
+            namabot: 'Bakulbot',
+            namaowner: 'Bakulcilok',
+            instagram: 'halim.rohmana',
             youtube : 'Gak Punya'
         }
     }
